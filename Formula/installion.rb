7be948a127293ec974a16d6c122178d498efc061
Jon Suderman <suderman@gmail.com>
2012-06-22 11:56:00 -0600

    Remove forumula

--- Formula/installion.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class Installion < GithubGistFormula
-  url 'https://raw.github.com/suderman/installion/master/installion'
-  homepage 'https://github.com/suderman/installion'
-  md5 'b92e835e120b4b3762b058944a885030'
-  version '0.1'
-end
