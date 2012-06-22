ed26429f2010ae03d1473ae6589bdac1e51d1b55
Jon Suderman <suderman@gmail.com>
2012-06-22 11:21:41 -0600

    Homebrew forumula

---
+++ Formula/installion.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class Installion < Formula
+  url 'https://raw.github.com/suderman/installion/master/installion'
+  homepage 'https://github.com/suderman/installion'
+  md5 'b92e835e120b4b3762b058944a885030'
+end
