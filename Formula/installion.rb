137ccf708e8afec9dc5521f22bc296ec5c0ca1ec
Jon Suderman <suderman@gmail.com>
2012-06-22 11:30:09 -0600

    Add version to forumula

--- Formula/installion.rb
+++ Formula/installion.rb
@@ -4,4 +4,5 @@ class Installion < Formula
   url 'https://raw.github.com/suderman/installion/master/installion'
   homepage 'https://github.com/suderman/installion'
   md5 'b92e835e120b4b3762b058944a885030'
+  version '0.1'
 end
