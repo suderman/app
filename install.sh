c12fc64c8d712f81844d41003a22cf8bcc9ec950
Jon Suderman <suderman@gmail.com>
2013-01-07 14:10:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 2012 Jon Suderman
+# 2013 Jon Suderman
 # https://github.com/suderman/symlink/
 
 # Open a terminal and run this command:
@@ -9,13 +9,17 @@
 # Ensure /usr/local/bin exists
 if [ ! -d "/usr/local" ]; then
   sudo mkdir -p /usr/local/bin
-  sudo chown `whoami`:admin /usr/{local,local/bin}
+  ULB_OWNER=`whoami`:`ls -ld /usr | awk '{print $4}'`
+  sudo chown $ULB_OWNER /usr/{local,local/bin}
 fi
 
 # Download app into /usr/local/bin
-curl https://raw.github.com/suderman/app/master/app -o /usr/local/bin/app
+sudo curl https://raw.github.com/suderman/app/master/app -o /usr/local/bin/app
 
 # Set permissions
-chmod +x /usr/local/bin/app
+ULB_OWNER=`ls -ld /usr/local/bin | awk '{print $3}'`:`ls -ld /usr/local/bin | awk '{print $4}'`
+sudo chown $ULB_OWNER /usr/local/bin/app 
+sudo chmod +x /usr/local/bin/app
 
 echo "Installed app to /usr/local/bin"
+
